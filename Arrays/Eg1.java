0ea352749907da0a45aebbc1d17ce2965a643d62
ZaheerShaik999 <zaheershaik529@gmail.com>
2024-10-12 21:11:52 +0530

    Add files via upload

---
+++ Arrays/Eg1.java
@@ -0,0 +1,33 @@
+package NEWN;
+
+public class Eg1 {
+	
+
+
+
+		//Arrays: Arrays are fixed in size, Arrays contains homogenious elements
+		
+		//length() Method and length property
+		
+		public static void main(String[] args) {
+			
+			//Task 1: Create a array and provide elements to it
+			int i[] = {10, 20, 30, 40, 50};
+			//          0   1   2   3   4   5 ...
+			
+			System.out.println(i.length); // 5
+			System.out.println(i[0]); // 10
+			System.out.println(i[1]); // 20
+			System.out.println(i[2]); // 30
+			System.out.println(i[3]); // 40
+			System.out.println(i[4]); // 50
+			//System.out.println(i[5]); // java.lang.ArrayIndexOutOfBoundsException
+			
+		}
+		
+		
+		
+		
+	}
+
+
