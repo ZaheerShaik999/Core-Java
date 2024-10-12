330d4a9911aed15de9ca9e8e0404a3315460c1f9
ZaheerShaik999 <zaheershaik529@gmail.com>
2024-10-12 21:16:32 +0530

    Add files via upload

---
+++ Arrays/Eg2.java
@@ -0,0 +1,42 @@
+package NEWN;
+
+public class Eg2 {
+	
+		public static void main(String[] args) {
+			
+			//Task 2: Declare an array and size first, then provide elements later
+			int i[] = new int[5];
+			System.out.println(i.length); // 5
+			
+			System.out.println(i[0]); // 0
+			System.out.println(i[1]); // 0
+			System.out.println(i[2]); // 0
+			System.out.println(i[3]); // 0
+			System.out.println(i[4]); // 0
+			
+			i[0] = 100;
+			i[1] = 200;
+//			i[2] = 300;
+			i[3] = 400;
+			i[4] = 500;
+			
+			System.out.println(i[0]); // 100
+			System.out.println(i[1]); // 200
+			System.out.println(i[2]); // 0
+			System.out.println(i[3]); // 400
+			System.out.println(i[4]); // 500
+			
+			
+		}
+	
+			
+		}
+		
+		
+		
+		
+	
+
+
+
+
