352a69d5fcb39965b7417d2b7c6fd77c028eeabc
ZaheerShaik999 <zaheershaik529@gmail.com>
2024-10-12 21:17:59 +0530

    Add files via upload

---
+++ Arrays/Eg3.java
@@ -0,0 +1,18 @@
+package NEWN;
+
+public class Eg3 {
+	
+
+		public static void main(String[] args) {
+			
+			//length variable
+			int a[] = {10, 20, 30, 40, 50};
+			System.out.println(a[0]); // 10
+			
+			System.out.println(a.length-1); //4
+			System.out.println(a.length * 4); //20
+			System.out.println(a.length);
+			
+		}
+	}
+
