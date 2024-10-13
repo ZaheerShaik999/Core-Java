328619bd4f4294ce4e0293ab3f18f573f2c60e12
ZaheerShaik999 <zaheershaik529@gmail.com>
2024-10-13 09:37:31 +0530

    Add files via upload

---
+++ MultiThreading/Eg2.java
@@ -0,0 +1,30 @@
+package NEWN;
+
+public class Eg2 {
+	
+	
+
+		public static void main(String[] args) {
+			
+			Eg2 eg2 = new Eg2();
+			eg2.run();
+		}
+		
+		//This is not a MultiThreading run() Method
+		public void run() {
+			
+			System.out.println("Run() Method");
+		}
+	}
+			
+			
+		
+		
+		
+		
+		
+	
+
+
+
+
