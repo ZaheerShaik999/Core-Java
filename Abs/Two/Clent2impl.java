4783f66a46dd49fcb515fb0ccd67e53cd5dbc937
Shaik Zaheer Basha <zaheershaik529@gmail.com>
2024-10-15 11:05:08 +0530

    Create Clent2impl.java

---
+++ Abs/Two/Clent2impl.java
@@ -0,0 +1,11 @@
+package com.dl.two;
+
+public class Client2Impl extends Client2 {
+
+	public static void main(String[] args) {
+		
+		//Single L 
+		Client2Impl client2Impl = new Client2Impl();
+		System.out.println(client2Impl.a);
+	}
+}
