530720f5d4ebab3b183155f7adc4fa351cb682ac
Shaik Zaheer Basha <zaheershaik529@gmail.com>
2024-10-15 10:57:57 +0530

    Create Clent.java

---
+++ FinalKeyword/Methods/Clent.java
@@ -0,0 +1,25 @@
+package com.dl.method;
+
+//If a method declared as final we cannot over-ride it
+class A{
+	
+	public final void m1() {
+		
+	}
+}
+
+class B extends A{
+	
+	public void m1() {
+		
+	}
+}
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+		Integer i;
+		
+	}
+}
