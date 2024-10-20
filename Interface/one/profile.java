d6e0f5e975382ba9a990a274bff4f8e453c48ffe
Shaik Zaheer Basha <zaheershaik529@gmail.com>
2024-10-20 12:40:38 +0530

    Create profile.java

---
+++ Interface/one/profile.java
@@ -0,0 +1,39 @@
+package com.dl.one;
+
+//Diff b/w abstract class and interface
+//Interface:
+//Interface contains all the abs methods
+public interface Profile {
+
+	//abs method
+	void m1();
+	
+	public abstract void m2();
+	
+	void m3();
+	
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	
+}
+
+
+//class A{
+//	
+//	//all methods with implementations
+//}
+//
+//abstract class B{
+//	
+//	//abs methods
+//	//concrete methods
+//	
+//}
+//
+//interface C{
+//	
+//	// abs methods
+//}
