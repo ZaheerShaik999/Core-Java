24ac41ece259255143052847d67f0b0aa3c7353b
Shaik Zaheer Basha <zaheershaik529@gmail.com>
2024-10-20 12:49:10 +0530

    Create Test.java

---
+++ Java Elements/Variables/Test.java
@@ -0,0 +1,16 @@
+package com.dl.variables;
+
+public class Test {
+	
+	int x = 100;
+
+	public static void main(String[] args) {
+		
+		int x = 200;
+		System.out.println(x);
+		
+		
+		System.out.println(new Test().x);
+		
+	}
+}
