0207a7caaf8fb058821b4d3018a79127e994f71e
Shaik Zaheer Basha <zaheershaik529@gmail.com>
2024-10-20 12:42:17 +0530

    Create Profile.java

---
+++ Interface/Two/Profile.java
@@ -0,0 +1,18 @@
+package com.dl.two;
+
+//Variables
+//Static V
+public interface Profile {
+
+	public static final int a = 10;
+	int b = 20;
+	int c = 30;
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
