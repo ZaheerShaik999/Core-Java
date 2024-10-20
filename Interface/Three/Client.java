795bb94a0fa0c89104199f1c73a279567ff65f80
Shaik Zaheer Basha <zaheershaik529@gmail.com>
2024-10-20 12:42:53 +0530

    Create Client.java

---
+++ Interface/Three/Client.java
@@ -0,0 +1,27 @@
+package com.dl.three;
+
+//using interface we can call mutiple interfaces at a time using mutiple inheritance
+interface A{
+	
+	
+}
+
+interface B{
+	
+	
+}
+
+interface C extends A, B{
+	
+	
+}
+
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
